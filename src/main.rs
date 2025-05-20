a1fa97821cb4fc7377a3f65fc9c46fa094e76142
Mario Salazar <mario.sb93@gmail.com>
2025-05-20 09:39:44 +0200

    Fix service address binding
     - Initially the uSVC was bound to 127.0.0.1 which worked for local tests. But:
       * Not having it listen to all interfaces would make it fail whenever
         setting it up as a pod/container.
       * Also, it wouldn't allow IPv6 binding.
     - Consequently bind address has been changed to [::]:8000 and if it fails due
       to lack of support for IPv6, it falls back to 0.0.0.0:8000
    
    Issue: #7

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use actix_web::{App, HttpServer};
 
+use std::io::ErrorKind;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
+
 mod health;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    const PORT: u16 = 8000;
+
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    HttpServer::new(|| {
+    let app_factory = || {
         App::new()
             .service(health::routes())
-    })
-        .bind_auto_h2c(("127.0.0.1", 8000))?
-        .workers(8)
-        .run()
-        .await
+    };
+    
+    // Attempt to bind to IPv6, fallback to IPv4 on AddrNotAvailable
+    let addr_v6 = SocketAddr::new(IpAddr::V6(Ipv6Addr::UNSPECIFIED), PORT);
+    let server = match HttpServer::new(app_factory).bind_auto_h2c(addr_v6) {
+        Ok(srv) => srv,
+        Err(err) if err.kind() == ErrorKind::AddrNotAvailable => {
+            eprintln!("IPv6 not available, falling back to IPv4");
+
+            let addr_v4 = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), PORT);
+            HttpServer::new(app_factory).bind_auto_h2c(addr_v4)?
+        }
+        Err(err) => return Err(err),
+    };
+
+    server.workers(8).run().await
 }
 
